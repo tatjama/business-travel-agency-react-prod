837ba86d19987a8456b34f246399709a9f7d32a8
Tanja <tanja120a@gmail.com>
2020-10-15 15:04:26 +0200

    Initial commit

---
+++ precache-manifest.4e4bb666ee4b03609e8e6ba7e700dfdf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d28ccf1922f049f4dc5717d32812400",
+    "url": "/business-travel-agency-react/index.html"
+  },
+  {
+    "revision": "1218a0559ab652e3d04e",
+    "url": "/business-travel-agency-react/static/css/main.e1d2532c.chunk.css"
+  },
+  {
+    "revision": "f7ae4d56b508f9409444",
+    "url": "/business-travel-agency-react/static/js/2.54dc9374.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/business-travel-agency-react/static/js/2.54dc9374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1218a0559ab652e3d04e",
+    "url": "/business-travel-agency-react/static/js/main.cb830dd6.chunk.js"
+  },
+  {
+    "revision": "14447cb765a62ed5ca7c",
+    "url": "/business-travel-agency-react/static/js/runtime-main.ad5df4a5.js"
+  },
+  {
+    "revision": "d657a6dc05a65af0c8da0c4d37d2bcd9",
+    "url": "/business-travel-agency-react/static/media/accomodation.d657a6dc.webp"
+  },
+  {
+    "revision": "24548d4be89cbf256c0faa3eec971c58",
+    "url": "/business-travel-agency-react/static/media/airplane.24548d4b.webp"
+  },
+  {
+    "revision": "0de2efb99c1264b67b6aa89a43b45b24",
+    "url": "/business-travel-agency-react/static/media/arrow-up.0de2efb9.svg"
+  },
+  {
+    "revision": "eba4120efee72ed461dc9f0b2de29ca1",
+    "url": "/business-travel-agency-react/static/media/avatar-dragan.eba4120e.webp"
+  },
+  {
+    "revision": "bb7ccc926f3b4c74db186de47bcce1d6",
+    "url": "/business-travel-agency-react/static/media/avatar-ivana.bb7ccc92.jpg"
+  },
+  {
+    "revision": "024705f94e9cddcda711590bd18f7434",
+    "url": "/business-travel-agency-react/static/media/avatar-jelena.024705f9.webp"
+  },
+  {
+    "revision": "bfd639719779bae641d844d4b78d56a8",
+    "url": "/business-travel-agency-react/static/media/breakfast.bfd63971.webp"
+  },
+  {
+    "revision": "7eee564e3657f17f0da404d8c39c41f5",
+    "url": "/business-travel-agency-react/static/media/city_lights.7eee564e.webp"
+  },
+  {
+    "revision": "8e8f90505abc48ae5c761dc78df88545",
+    "url": "/business-travel-agency-react/static/media/enjoy.8e8f9050.webp"
+  },
+  {
+    "revision": "a42e009155ece367c2d4d4863cb7f5e9",
+    "url": "/business-travel-agency-react/static/media/feedback.a42e0091.webp"
+  },
+  {
+    "revision": "5f964a5c3dfd389de1e2f9dcc5740d00",
+    "url": "/business-travel-agency-react/static/media/hotel.5f964a5c.webp"
+  },
+  {
+    "revision": "a5d655c6f2de21ec6237504f416a2aa1",
+    "url": "/business-travel-agency-react/static/media/logoavio1.a5d655c6.webp"
+  },
+  {
+    "revision": "e75aefda9b2c67db8febf8897d2d1a4d",
+    "url": "/business-travel-agency-react/static/media/metropolis.e75aefda.webp"
+  },
+  {
+    "revision": "5c46034f0867e924e19147f3e4875538",
+    "url": "/business-travel-agency-react/static/media/providers.5c46034f.webp"
+  },
+  {
+    "revision": "16076c86ebcfd70ce20ddcabf89cf37a",
+    "url": "/business-travel-agency-react/static/media/suitcases.16076c86.webp"
+  },
+  {
+    "revision": "6438894818698f70f61906989c42135d",
+    "url": "/business-travel-agency-react/static/media/travel-and-tourism.64388948.png"
+  },
+  {
+    "revision": "af8a393b5e12b069d6e0880ab1fc8107",
+    "url": "/business-travel-agency-react/static/media/world.af8a393b.webp"
+  }
+]);
